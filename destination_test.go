690906bad523501308cf71bbd2006c9ed229aaa4
Parikshit Gothwal <parikshit.g@gopherslab.com>
2025-01-07 11:14:13 +0530

    fix: pr comment

--- destination_test.go
+++ destination_test.go
@@ -239,7 +239,7 @@ func prepareServerAndDestination(t *testing.T, expected []opencdc.Record) (sdk.D
 	return dest, ctx
 }
 
-func startTestServer(t *testing.T, lis net.Listener, enableMTLS bool, _ []opencdc.Record) { //nolint:funlen // ignore.
+func startTestServer(t *testing.T, lis net.Listener, enableMTLS bool, _ []opencdc.Record) {
 	is := is.New(t)
 	ctrl := gomock.NewController(t)
 	serverOptions := make([]grpc.ServerOption, 0, 1)
